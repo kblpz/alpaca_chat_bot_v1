915b7762cf1ad3763c88118abe7e8a3ff80a2fc8
kblp <kblp@me.com>
2025-06-30 23:10:42 +0300

    test

--- chatbot_app.py
+++ chatbot_app.py
@@ -4,7 +4,38 @@ from bot import chat_with_alpaca
 st.set_page_config(page_title="Alpaca Chatbot")
 st.title("Chat with Alpaca")
 #st.markdown("Ask anything about *Alpaca*.")
-
+hide_streamlit_style = """
+                <style>
+                div[data-testid="stToolbar"] {
+                visibility: hidden;
+                height: 0%;
+                position: fixed;
+                }
+                div[data-testid="stDecoration"] {
+                visibility: hidden;
+                height: 0%;
+                position: fixed;
+                }
+                div[data-testid="stStatusWidget"] {
+                visibility: hidden;
+                height: 0%;
+                position: fixed;
+                }
+                #MainMenu {
+                visibility: hidden;
+                height: 0%;
+                }
+                header {
+                visibility: hidden;
+                height: 0%;
+                }
+                footer {
+                visibility: hidden;
+                height: 0%;
+                }
+                </style>
+                """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 # Initialize session state
 if "history" not in st.session_state:
     st.session_state.history = []
